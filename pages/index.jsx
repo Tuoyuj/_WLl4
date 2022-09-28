4f5e58a3cbff4cde45efedb47c34a7fc12b98d70
Tuoyuj <aeafnd257@gmail.com>
2022-09-28 11:09:34 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "iplNi_3ZyquBGUw4iy54Df5a3XVke86_"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "iplNi_3ZyquBGUw4iy54Df5a3XVke86_"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
